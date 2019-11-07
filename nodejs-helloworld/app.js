9a6a8697e9f8e17367073c5145ddc9c382d4f165
Student User <student@workstation.lab.example.com>
2019-11-07 15:17:17 +0000

    Se modifica mensaje

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('VIVA PERON, EVA, NESTOR Y CRISTINA!\n');
 });
 
 app.listen(8080, function () {
